1af63b3d458c2b47655872224e0b2ff9e8f783cc
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-01-18 22:36:32 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,24 +4,20 @@ var cards = ["Queen", "Queen", "King", "King"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-	cardsInPlay.push('cardOne');
-
-console.log("User flipped Queen");
-
-var cardTwo = cards[2];
-	cardsInPlay.push('cardTwo');
-
-console.log("User flipped King");
-
-
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 	} 
 		else {
 			alert("Sorry, try again.");
 		}
-	}
-	
+}
 
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	}
+
+flipCard(0);
+flipCard(2);
