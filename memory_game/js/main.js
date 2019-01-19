c114131a25409d33da565b31ed984783a309cb5a
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-01-19 15:43:48 -0800

    created card object

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,36 @@
 console.log("Up and running!");
 
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+
+	{
+		rank: "King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+
+];
 
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 	} 
@@ -14,8 +40,13 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	
+	cardsInPlay.push(cards[cardId].rank);
+
 	checkForMatch();
 	}
 
